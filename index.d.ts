a694259740526ec36d39a48c598d5cce4f8f49a9
Matt Morrissette <yinzara@gmail.com>
2019-09-11 21:41:25 -0700

    Add missing types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for express-winston 3.0
+// Type definitions for express-winston 4.0
 // Project: https://github.com/bithavoc/express-winston#readme
 // Definitions by: Alex Brick <https://github.com/bricka>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
